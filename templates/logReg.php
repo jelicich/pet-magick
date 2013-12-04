4a7bb06c7bf431ae76846ee67b9337d2eedae702
jelicich <jelicich.e@gmail.com>
2013-12-04 18:19:52 -0300

    css popup registro

--- templates/logReg.php
+++ templates/logReg.php
@@ -12,35 +12,38 @@
 			?>
 			<form class="clearfix" id="form-login">
 				
+				<div class="half">
+					<div>
+						<label for="name">Name</label>
+						<input type="text" name="name" id="name" placeholder="name" style="width:100%"/>
+					</div>
 
-				<div>
-					<label for="name">name</label>
-					<input type="text" name="name" id="name" placeholder="name" style="width:100%"/>
+					<div>
+						<label for="lastname">Lastname</label>
+						<input type="text" name="lastname" id="lastname" placeholder="lastname" style="width:100%"/>
+					</div>
+
+					<div>
+						<label for="nickname">Nickname</label>
+						<input type="text" name="nickname" id="nickname" placeholder="nickname" style="width:100%"/>
+					</div>
 				</div>
 
-				<div>
-					<label for="lastname">lastname</label>
-					<input type="text" name="lastname" id="lastname" placeholder="lastname" style="width:100%"/>
-				</div>
+				<div class="half">
+					<div>
+						<label for="email">e-mail</label>
+						<input type="text" name="email" id="email" placeholder="email" style="width:100%"/>
+					</div>
 
-				<div>
-					<label for="nickname">nickname</label>
-					<input type="text" name="nickname" id="nickname" placeholder="nickname" style="width:100%"/>
-				</div>
+					<div>
+						<label for="password">Password</label>
+						<input type="text" name="password" id="password" placeholder="password" style="width:100%"/>
+					</div>
 
-				<div>
-					<label for="email">email</label>
-					<input type="text" name="email" id="email" placeholder="email" style="width:100%"/>
-				</div>
-
-				<div>
-					<label for="password">password</label>
-					<input type="text" name="password" id="password" placeholder="password" style="width:100%"/>
-				</div>
-
-				<div>
-					<label for="password2">password-2</label>
-					<input type="text" name="password2" id="password2" placeholder="password2" style="width:100%"/>
+					<div>
+						<label for="password2">Confirm password</label>
+						<input type="text" name="password2" id="password2" placeholder="password2" style="width:100%"/>
+					</div>
 				</div>
 
 				<div id="country-wrapper">
@@ -65,7 +68,7 @@
 	     			</select>
 	     		</div>
 
-	     		<input type="button" id="reg" value="Sign up!" />
+	     		<input type="button" id="reg" value="Sign up!" class="btn btn-danger" />
 				<input type="hidden" name="token" id="token" value=<?php echo '"'. $_SESSION['token'] . '"'; ?> />
 
 			</form>

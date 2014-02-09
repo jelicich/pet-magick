310119901d70d9af2d4d78aa64d0ff3e1bd8e4ae
jelicich <jelicich_e@hotmail.com>
2014-02-09 01:55:14 -0300

    merge puto de video
    asd

--- templates/videoPosta.php
+++ templates/videoPosta.php
@@ -1,8 +1,19 @@
+<?php
+        $p = new BOPets;
+        
+        if(isset($_GET['p']))
+         $petID = $_GET['p'];
+        elseif(isset($_POST['p']))
+         $petID = $_POST['p'];
+        
+        $p->getPetData($petID);
+       ?>
 
 <?php 
 $a = $v->getVideo();
 ?>
 
+<<<<<<< HEAD
 <a class="petVideo video" href= <?php  echo 'video/'.$a['VIDEO']; ?> >
 	<!--Puse un div provisorio asi no llorisqueas jajaj. Cuando sepamos como vamos a tomar los valores con js y como mostrar el video lo acomodamos como corresponde. Q opinas? -->
 	<span class='wrapper-play'>
@@ -13,5 +24,31 @@ $a = $v->getVideo();
 	<span class="video-last-caption">
 		<h3><?php echo $a['TITLE']; ?></h3>
 		<span><?php echo $a['CAPTION']; ?></span>
+=======
+
+<?php 
+	$a = $v->getVideoByPet($p->getId());
+?>
+
+<a class="petVideo video" href= <?php  echo 'video/'.$a[0]['VIDEO']; ?> >
+	<!--Puse un div provisorio asi no llorisqueas jajaj. Cuando sepamos como vamos a tomar los valores con js y como mostrar el video lo acomodamos como corresponde. Q opinas? -->
+	<span class='wrapper-play'>
+		<span class="play"></span>
+		<img src= <?php echo '"video/'.$a[0]['THUMBNAIL'].'"'; ?> class="thumb-big video-thumb"/>
+	</span>
+
+	<span class="video-last-caption">
+		<h3><?php echo $a[0]['TITLE']; ?></h3>
+		<span><?php echo $a[0]['CAPTION']; ?></span>
+>>>>>>> 0f7acf2983737afb841f735f675221f103a5ddc3
 	</span>
 </a>
+<?php
+	if($p->isOwn())
+	{
+		echo '<a href="#'.$p->getId().'" class="btn" id="delete-pet-video">Delete video</a>';
+	}
+?>
+	<script type="text/javascript">
+	deleteVideo();
+	</script>

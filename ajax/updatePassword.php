207df124a00004db8d0539b3d08d5a5a200c7488
Esteban Jelicich <jelicich.e@gmail.com>
2014-03-24 18:56:25 -0300

    cambio psswd en wp

--- ajax/updatePassword.php
+++ ajax/updatePassword.php
@@ -15,5 +15,9 @@ $data = array(
 
 $u->updatePassword($data);
 
+//WP user
+include_once '../blog/wp-load.php';
+//wp_set_password($_POST['password'],$_POST['user_id']);
+wp_update_user(array('ID' => $_POST['user_id'], 'user_pass' => $_POST['newPassword']));
 
 ?>

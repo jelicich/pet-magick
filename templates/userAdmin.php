aeafff08c9271fe8b082469b57bda70b3130737d
Esteban Jelicich <jelicich.e@gmail.com>
2014-04-18 20:31:16 -0300

    delete user btw

--- templates/userAdmin.php
+++ templates/userAdmin.php
@@ -107,7 +107,7 @@
 						 	<div class="tab-pane" id="deleteTab">
 
 								<p>If you don't want to keep your Pet Magick account in Pet Magick you can delete it by clicking the button bellow. <br/>Please take into account that all the information (including media files) will be deleted as well and it won't may be recovered.</p>
-								<button class="btn btn-danger">Delete Account</button>
+								<button class="btn btn-danger" id="btn-delete-account">Delete Account</button>
 
 						 	</div>		
 
@@ -122,7 +122,46 @@
 ?>
 			<script type="text/javascript">
 				updatePassword(<?php echo $_SESSION['id']; ?>);
+				function deleteAccount()
+				{
+					if(byid('btn-delete-account'))
+					{
+						byid('btn-delete-account').onclick = function()
+						{
+							byid('modal-edit-container').style.display='block';
+							var html = '<div id="modal-edit" class="edit-scrollable"><div class="mod-header"><h2>Delete Account</h2></div><div class="mod-content"><p>Are you sure you want to delete your account?</p><button class="btn btn-danger" id="confirm-delete">Yes</button><button class="btn" id="cancel-delete">No</button></div>';
+							printEdit('modal-edit',html);
+							byid('cancel-delete').onclick = function(){cancelDelete();}
+							byid('confirm-delete').onclick = function(){confirmDelete();}
+						}
+					}
+				
+					function cancelDelete()
+					{
+						byid('modal-edit-container').style.display='none';
+						printEdit('modal-edit','<img class="loading" src="img/loading.gif" width="208" height="13" />');
+					}
+					function confirmDelete()
+					{
+						ajax('POST', 'ajax/login.php', printEditDeleteUsr, vars, true);
+					}
+					function printEditDeleteUsr()
+					{
+						printEdit('modal-edit', this.responseText);
+						setTimeout(function()
+						{
+							window.location.href='www.petmagick.com';
+						}, 3000);
+					}
+				}
+				
+				deleteAccount();
+
+
+
+
 			</script>
+			
 <?php
 }
 ?>

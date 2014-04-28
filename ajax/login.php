fe8efe9ba7f93d4e19f8adf282b284a513169d76
Esteban Jelicich <jelicich.e@gmail.com>
2014-04-27 23:49:30 -0300

    login

--- ajax/login.php
+++ ajax/login.php
@@ -33,7 +33,7 @@ if($user->login(array($_POST['email'],$_POST['password'], $_SESSION['token']))) 
 	$creds = array();
 	$creds['user_login'] = $_POST['email'];
 	$creds['user_password'] = $_POST['password'];
-	$creds['remember'] = false;
+	$creds['remember'] = true;
 	$user = wp_signon( $creds, false );
 	if ( is_wp_error($user) )
 		echo $user->get_error_message();			   
